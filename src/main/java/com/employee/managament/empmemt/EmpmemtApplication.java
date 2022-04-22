f7c8cd927612498ad22579d9a6c911b858fc47be
SudheeshPSuresh <78212044+SudheeshPSuresh@users.noreply.github.com>
2022-04-22 13:58:22 +0530

    Added bolier plate

---
+++ src/main/java/com/employee/managament/empmemt/EmpmemtApplication.java
@@ -0,0 +1,13 @@
+package com.employee.managament.empmemt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmemtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmemtApplication.class, args);
+	}
+
+}
